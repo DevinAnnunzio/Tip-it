ece36c2cab66824dabd71ad4f9d5e97840fca6ba
DevinAnnunzio <annunzio.da@gmail.comgit config --global user.name DevinAnnunzio git config --global user.name DevinAnnunzio git config --global user.email annunzio.da@gmail.com>
2022-11-22 14:55:59 +0900

    Finished temporarily

--- app/src/androidTest/java/com/annunzio/tipit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/annunzio/tipit/ExampleInstrumentedTest.kt
@@ -1,10 +1,18 @@
 package com.annunzio.tipit
-
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Matchers.containsString
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 import org.junit.Assert.*
 
@@ -15,10 +23,28 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.annunzio.tipit", appContext.packageName)
+    }
+
+
+    @Test
+    fun calculate_20_percent_tip() {
+        onView(withId(R.id.tietServiceCost))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.btnCalculate)).perform(click())
+
+        onView(withId(R.id.tvTipResult))
+            .check(matches(withText(containsString("10"))))
     }
 }
--- app/src/main/java/com/annunzio/tipit/MainActivity.kt
+++ app/src/main/java/com/annunzio/tipit/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.annunzio.tipit
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.viewbinding.ViewBinding
+import com.annunzio.tipit.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    //Viewbinding obj has references to all views with an ID
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnCalculate.setOnClickListener{ calculateTip() }
+
+        binding.tietServiceCost.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
+    }
+
+    private fun calculateTip() {
+        //the text attribute of an EditText is an Editable, because it represents text that can be changed.
+        val costOfServiceText = binding.tietServiceCost.text.toString()
+        val costOfService = costOfServiceText.toDoubleOrNull()
+        if (costOfService == null) {
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage = when(binding.rgTipOptions.checkedRadioButtonId){
+            R.id.rbTwentyPercentTip -> 0.20
+            R.id.rbEighteenPercentTip -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * costOfService
+
+        if(binding.sRoundUpTip.isChecked){
+            tip = kotlin.math.ceil(tip)
+        }
+
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double){
+        val formattedTip = NumberFormat.getInstance().format(tip)
+        binding.tvTipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
